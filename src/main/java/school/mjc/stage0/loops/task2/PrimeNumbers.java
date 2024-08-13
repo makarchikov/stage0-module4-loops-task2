f2300c069fdf439bad5add576569b0507a7c5e42
salvadordali99 <152178723+salvadordali99@users.noreply.github.com>
2024-08-13 23:45:45 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -13,11 +13,13 @@ public class PrimeNumbers {
             for (int i = 2; i <= Math.sqrt(current); i++) {
                 if (current % i == 0) {
                     isPrime = false;
+                    break;
                 }
             }
             if (isPrime) {
-                System.out.println(current++);
+                System.out.println(current);
             }
+            current++;
         }
     }
 }
